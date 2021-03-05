a915113a6a534fef5cc5f075406e6e61fb4d4340
Jerson Janke <jersonjanke@gmail.com>
2021-03-05 13:47:36 +0000

    create Input

--- src/Form.js
+++ src/Form.js
@@ -1,58 +1,57 @@
-import React, { useState } from "react";
+import React, { useState } from "react"
+import Input from './Input'
 
 const initialEntryState = {
-  recordName: "",
-  artistName: "",
-  description: "",
+    recordName: "",
+    artistName: "",
+    description: "",
 };
 
 const Form = ({ onSubmit }) => {
-  const [entry, setEntry] = useState(initialEntryState);
+    const [entry, setEntry] = useState(initialEntryState)
 
-  const onChangeHandle = (e) => {
-    setEntry({
-      ...entry,
-      [e.target.name]: e.target.value,
-    });
-  };
+    const onChangeHandle = (e) => {
+        setEntry({
+            ...entry,
+            [e.target.name]: e.target.value,
+        });
+    };
 
-  const onSubmitHandler = (e) => {
-    e.preventDefault();
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
 
-    if (!entry.recordName || !entry.artistName) {
-      return;
-    }
+        if (!entry.recordName || !entry.artistName) {
+            return;
+        }
 
-    onSubmit({ ...entry });
-    setEntry(initialEntryState);
-  };
+        onSubmit({ ...entry });
+        setEntry(initialEntryState);
+    };
 
-  return (
-    <form onSubmit={onSubmitHandler}>
-      <label htmlFor="recordName">Record name</label>
-      <input
-        id="recordName"
-        name="recordName"
-        onChange={onChangeHandle}
-        value={entry.recordName}
-      />
-      <label htmlFor="artistName">Artist name</label>
-      <input
-        id="artistName"
-        name="artistName"
-        onChange={onChangeHandle}
-        value={entry.artistName}
-      />
-      <label htmlFor="description">Description</label>
-      <textarea
-        id="description"
-        name="description"
-        onChange={onChangeHandle}
-        value={entry.description}
-      />
-      <button type="submit">Add</button>
-    </form>
-  );
+    return (
+        <form onSubmit={onSubmitHandler}>
+            <Input
+                labelText="Record name"
+                name="recordName"
+                onChange={onChangeHandle}
+                value={entry.recordName}
+            />
+            <Input
+                labelText="Artist name"
+                name="artistName"
+                onChange={onChangeHandle}
+                value={entry.artistName}
+            />
+            <Input
+                labelText="Description"
+                type="textarea"
+                name="description"
+                onChange={onChangeHandle}
+                value={entry.description}
+            />
+            <button type="submit">Add</button>
+        </form>
+    );
 };
 
 export default Form;
---
+++ src/Input.js
@@ -0,0 +1,19 @@
+import React, { useRef, Fragment } from "react"
+import uniqid from 'uniqid'
+
+const Input = ({ type, labelText, ...props }) => {
+
+    const id = useRef(uniqid())
+
+    return (
+        <Fragment>
+            <label htmlFor={id.current}>{labelText}</label>
+            {type === 'textarea' ?
+                <textarea id={id.current} {...props}></textarea>
+                : <input id={id.current} {...props} />}
+
+        </Fragment>
+    )
+}
+
+export default Input
